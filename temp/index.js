a1c364d36b2b2326737bdfe00bd074430a488a06
Brian Anstett <baanstet@oakland.edu>
2018-12-29 23:00:07 -0500

    v1 of imgur puller

--- temp/index.js
+++ temp/index.js
@@ -8,71 +8,84 @@ const app = express();
 const port = 3000;
 let images = [];
 
-app.use('/static', express.static(path.join(__dirname, 'images')));
+app.use('/static/images', express.static(path.join(__dirname, 'images')));
 
-app.get('/', (req, res, next)=>{
-
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './html/index.html'));
 })
 
-app.get('/images', (req, res, next)=>{
-  fs.readdir(path.join(__dirname, 'images'), (error, files)=>{
-    files.forEach((file, index)=>{
-      files[index] = `/static/${file}`
+app.get('/images', (req, res, next) => {
+  fs.readdir(path.join(__dirname, 'images'), (error, files) => {
+    files.forEach((file, index) => {
+      files[index] = `/static/images/${file}`
     })
     return res.send(files);
   })
 })
 
-app.get('/imgur/image', (req, res, next)=>{
+app.get('/imgur/image', (req, res, next) => {
   const imageIndex = Math.floor(Math.random() * Math.floor(images.length));
-  imgur.getImageMetaData(images[imageIndex], (error, imageMetaData)=>{
-    if(error){
+  imgur.getImageMetaData(images[imageIndex], (error, imageMetaData) => {
+    if (error) {
       console.log(error);
-      return res.send("ERROR").status(502);
+      return res.status(502).send("ERROR");
     }
 
-    imgur.downloadImage(imageMetaData.link, (error, image)=>{
-      if(error){
+    imgur.downloadImage(imageMetaData.link, (error, image) => {
+      if (error) {
         console.log(error);
-        return res.send("ERROR").status(502);
+        return res.status(502).send("ERROR");
       }
       images.splice(imageIndex, 1);
-      res.send({path: `/static/${image.fileName}`})
+      res.send({ path: `/static/images/${image.fileName}` })
     });
   })
-  
+
 })
 
-function init(){
-  if(! fs.existsSync('images/')){
+app.delete('/imgur/image/:image', (req, res, next) => {
+  const image = req.params.image;
+  console.log(image);
+  fs.unlink(path.join(__dirname, `./images/${image}`), err => {
+    if (err) {
+      console.log(err);
+      return res.status(502).send(`Could not delete file ${image}`);
+    }
+    return res.send(`Removed file ${image}`);
+  })
+
+})
+
+function init() {
+  if (!fs.existsSync('images/')) {
     console.log('No images directory found. \n Creating one.')
     fs.mkdirSync('images');
   }
-  if(! fs.existsSync('imageIDs.json')){
+  if (!fs.existsSync('imageIDs.json')) {
     console.log('Pulling Imgur image IDs');
 
-    imgur.getImageIds((error, images)=>{
-      if(error){
+    imgur.getImageIds((error, images) => {
+      if (error) {
         console.log(error);
         rocess.exit();
       }
       console.log('Finished pulling image IDs');
       fs.writeFileSync('imageIDs.json', JSON.stringify(images));
 
-      return app.listen(3000, ()=>{
+      return app.listen(3000, () => {
         console.log(`Imgur puller listening port ${port}`);
       })
     })
-    
-  }else{
+
+  } else {
     images = JSON.parse(fs.readFileSync('imageIDs.json'));
-    return app.listen(3000, ()=>{
+    return app.listen(3000, () => {
       console.log(`Imgur puller listening port ${port}`);
     })
   }
 }
 
-function closeServer(server){
+function closeServer(server) {
   console.log("writing image IDs back to disk");
   fs.writeFileSync('imageIDs.json', JSON.stringify(images));
   console.log("Finished writing iamge IDs to disk");
@@ -82,12 +95,12 @@ function closeServer(server){
 const server = init();
 
 // listen for TERM signal .e.g. kill
-process.on ('SIGTERM', ()=>{
-  closeServer(server)  
+process.on('SIGTERM', () => {
+  closeServer(server)
 });
 
 
 // listen for INT signal e.g. Ctrl-C
-process.on ('SIGINT', ()=>{
-  closeServer(server)  
+process.on('SIGINT', () => {
+  closeServer(server)
 });
